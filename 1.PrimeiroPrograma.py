bda53e4284b46d448d5bc550da832efcc63104ad
LucasQuintela <lucasquintela.dev@gmail.com>
2021-07-03 12:35:03 -0300

    inicio do curso

---
+++ 1.PrimeiroPrograma.py
@@ -0,0 +1,2 @@
+print('Primeiro programa')
+
---
+++ 2.TiposBasicos.py
@@ -0,0 +1,11 @@
+"""
+Tipos basicos
+"""
+print(True)  #bolean
+print('Tipos basicos')  # string
+print(1.2 + 3)  #Float
+print('Você e', 3 * 'muito' + 'Legal !')
+# print(3 + '3') -> ambiguidade
+print([1, 2, 3, 4])
+print({'nome': 'Lucas', 'idade': 24})
+
